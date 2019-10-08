dffccd5afdbdc829867df1d4c5eb2a9835830e52
Parth Patel <47762364+parth-patel7@users.noreply.github.com>
2019-10-08 12:06:11 -0400

    Update ViewController.java

--- controller/ViewController.java
+++ controller/ViewController.java
@@ -1,6 +1,9 @@
+/**
+ * @author Parth Patel
+ */
+
+
 package controller;
-
-
 
 import java.io.IOException;
 import java.io.Writer;
@@ -16,6 +19,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -26,14 +30,11 @@ import javafx.scene.control.Label;
 
 
 
-/**
- * @author Parth Patel
- */
 public class ViewController {
 
 
-
 	@FXML public AnchorPane mainAnchorPane;	
+	@FXML private Button cancel;
 	@FXML private Label nameDetail;	
 	@FXML private Label artistDetail;	
 	@FXML private Label albumDetail;	
@@ -54,8 +55,7 @@ public class ViewController {
 
 	public static ArrayList <songObj> songList = new ArrayList <songObj>();
 	private ObservableList <String> obslist = FXCollections.observableArrayList();
-	@FXML
-	ListView <String> listview = new ListView<String>();
+	@FXML ListView <String> listview = new ListView<String>();
 
 
 
@@ -69,6 +69,7 @@ public class ViewController {
 
 	@FXML
 	public void initialize() {
+
 		if(databaseAlreadyExists() == true) {	
 			retrieveDataFromFile();	
 		}	
@@ -77,50 +78,31 @@ public class ViewController {
 
 
 
-
-
 	/**
 	 * @param event
 	 * @throws IOException
+	 * 
+	 * When the save button is pressed this method activates,
+	 * it makes new object, inserts the new song in correct order
+	 * and then clears the text (input) fields.
 	 */
 	@FXML
 	public void saveButton(ActionEvent event) throws IOException  {
 
 		songObj newSong = new songObj("","","","");
+		newSong = makeNewObject(newSong);
 
-		
-		// As the name and artist are required we check if the related fields are empty
-		// if yes then warning alert and exit. 
-		if(artistTextField.getText().isEmpty() || nameTextField.getText().isEmpty()) {
-			Alert alert = new Alert(AlertType.WARNING ,  "Name or Artist of the song is MISSING MotherFUcker", ButtonType.OK);
-			alert.showAndWait();
-			if (alert.getResult() == ButtonType.OK) {
-				clearTextFeilds();
-				return;
-			}
+		if(newSong == null) {
+			return;
 		}
 
-		// Info requirements are met and thus adding new song to playlsit.
-		
-		if(!artistTextField.getText().isEmpty() && !nameTextField.getText().isEmpty()) {
-			newSong.name = nameTextField.getText();
-			newSong.artist = artistTextField.getText();
-		}
-
-		if(!albumTextField.getText().isEmpty()) {
-			newSong.album = albumTextField.getText();
-		}
-		if(!yearTextField.getText().isEmpty()) {
-			newSong.year = yearTextField.getText();
-		}
 
 		insertNewSong(newSong);
 
 		// Check if the user is editing or adding song.
 		// If editing then remove the current song from the listview and add the new one.
 		if(counter == 0) {
-			removeFromList();	
-			counter = -1;
+			removeFromList();
 		}
 
 		clearTextFeilds();
@@ -128,14 +110,109 @@ public class ViewController {
 
 
 
+	public void insertNewSong(songObj toInsert) {
+
+		int size = songList.size();
+
+		if(size == 0) {
+			songList.add(toInsert);
+			listview.getItems().add(toInsert.name + " , " + toInsert.artist);
+			return;
+		}
 
 
+		int i, temp =0;
+		// Adding the new song in alphabetical order.
+		mainloop:
+			for(i=0; i<size; i++) {
+
+				if(toInsert.name.compareToIgnoreCase(songList.get(i).name) < 0) {
+					toInsertHelper(i , toInsert);
+					return;
+				}
+
+				if ((songList.get(i).name.compareToIgnoreCase(toInsert.name) == 0) && 
+						(songList.get(i).artist.compareToIgnoreCase(toInsert.artist) ==0)) {
+
+					if(counter == 0) {
+						counter = -1;
+						return;
+					} else {
+						Alert alert = new Alert(AlertType.WARNING ,  "Song already EXISTS", ButtonType.OK);
+						alert.showAndWait();
+						return;
+
+					}
+				}
+
+				// If the name is same then we compare artist
+				// If artist is also same then it means that this is a
+				// duplicate song and therefore we throw an warning.
+				temp = i;
+				while( (temp < size) && (songList.get(i).name.compareToIgnoreCase(toInsert.name) == 0)){
+
+					if(songList.get(i).artist.compareToIgnoreCase(toInsert.artist) ==0) {
+						Alert alert = new Alert(AlertType.WARNING ,  "Song already EXISTS", ButtonType.OK);
+						alert.showAndWait();
+						return;
+					}
+
+					if(toInsert.artist.compareToIgnoreCase(songList.get(i).artist) < 0) {
+						toInsertHelper(temp , toInsert);
+						return;
+					}
+					temp++;
+					if(temp == i) {
+						break mainloop;
+					}
+				}
+			}
+
+
+		songList.add(toInsert);
+		listview.getItems().add(toInsert.name + " , " + toInsert.artist);
+	}
 
 
 
 
 	/**
+	 * @param toMake object
+	 * @return object
+	 * 
+	 * This method takes all the info about the song from the text fields
+	 * and makes an song object out of it.
+	 */
+	public songObj makeNewObject(songObj toMake) {
+
+		if(artistTextField.getText().isEmpty() || nameTextField.getText().isEmpty()) {
+			Alert alert = new Alert(AlertType.WARNING ,  "Name or Artist of the song is MISSING", ButtonType.OK);
+			alert.showAndWait();
+			if (alert.getResult() == ButtonType.OK) {
+				clearTextFeilds();
+				return null;
+			}
+		}
+
+		if(!artistTextField.getText().isEmpty() && !nameTextField.getText().isEmpty()) {
+			toMake.name = nameTextField.getText();
+			toMake.artist = artistTextField.getText();
+		}
+
+		if(!albumTextField.getText().isEmpty()) {
+			toMake.album = albumTextField.getText();
+		}
+		if(!yearTextField.getText().isEmpty()) {
+			toMake.year = yearTextField.getText();
+		}
+		return toMake;
+	}
+
+
+	/**
 	 * @param event
+	 * 
+	 * This method deletes the song (Confirms with the user before deleting)  
 	 */
 	@FXML
 	void deleteSong(ActionEvent event) {
@@ -153,10 +230,11 @@ public class ViewController {
 		deleteSongHelper();
 	}
 
-
+	// helper method for delete
 	public void deleteSongHelper() {
 
 		songObj target = getTargetObject();
+		int i = songList.indexOf(target);
 		songList.remove(target);
 		removeFromList();		
 
@@ -166,13 +244,28 @@ public class ViewController {
 			albumDetail.setText("");
 			yearDetail.setText("");
 		} else {
-			// After deletion next song should be displayed xx
+			if(i < songList.size()) {
+				listview.getSelectionModel().select(i);
+				viewDetailsHelper();
+			}
+			if(i == songList.size()) {
+				if(i-1 >= 0) {
+					listview.getSelectionModel().select(i-1);
+					viewDetailsHelper();
+				}
+			}
 		}
 	}
 
 
 
-	//When the use cancels midway the object is still deleted
+	
+	/**
+	 * @param event
+	 * 
+	 * This method runs when the user presses the EDIT button (Confirms with the user if they are
+	 * sure they want to edit a song)
+	 */
 	@FXML
 	void editSong(ActionEvent event) {
 
@@ -188,135 +281,66 @@ public class ViewController {
 			return;
 		}
 
-		String [] words = (listview.getSelectionModel().getSelectedItem().toString()).split("\\s*,\\s*");
-		String name = words[0];
+		songObj targetObj = getTargetObject();
+
+		nameTextField.setText(targetObj.name);
+		artistTextField.setText(targetObj.artist);
+		albumTextField.setText(targetObj.album);
+		yearTextField.setText(targetObj.year);
+
+		songObj newSong = new songObj("","","","");
+		newSong = makeNewObject(newSong);
 
 
-		int size = songList.size();
-
-		for(int i =0; i<size; i++) {
-			if(name.equals(songList.get(i).name)) {
-				if(words.length > 1) {
-					if(words[1].toString().equals(songList.get(i).artist)) {
-						// Match found in song object array list
-
-						songObj targetObj = songList.get(i);
-
-						nameTextField.setText(targetObj.name);
-						artistTextField.setText(targetObj.artist);
-						albumTextField.setText(targetObj.album);
-						yearTextField.setText(targetObj.year);
-
-						// you remove the obj here but what if someone calls cancel here
-						songList.remove(targetObj);
-
-						// get the new data and swap it with older data
-
-						songObj newSong = new songObj("","","","");
-
-						if(!nameTextField.getText().isEmpty()) {
-							newSong.name = nameTextField.getText();
-						} else {
-							// Throw Error
-						}
-
-						if(!artistTextField.getText().isEmpty()) {
-							newSong.artist = artistTextField.getText();
-						} else {
-							// Throw Error
-						}
-
-						if(albumTextField.getText() != null) {
-							newSong.album = albumTextField.getText();
-						}
-
-						if(yearTextField.getText() != null) {
-							newSong.year = yearTextField.getText();
-						}
-
-						break;
-					}
-				}
-			}
-		}
+		insertNewSong(newSong);
 	}
 
 
-	// Finish this	(CHECK FOR SAME SONGS)
-	public void insertNewSong(songObj toInsert) {
-
-		int size = songList.size();
-
-		System.out.println("Size = "+ size);
-
-		if(size == 0) {
-			songList.add(toInsert);
-			listview.getItems().add(toInsert.name + " , " + toInsert.artist);
-			return;
-		}
-
-		// Adding the new song in alphabetical order.
-		/*
-		for(int i=0; i<size; i++) {
-			if(toInsert.name.compareToIgnoreCase(songList.get(i).name) < 0) {
-				toInsertHelper(i , toInsert);
-				return;
-			}
-			else if (songList.get(i).name.compareToIgnoreCase(toInsert.name) == 0) {
-				// check next name if it is same
-				// if yes then compare artist and insert
-				int temp = i;
-				while( (temp < size) && (songList.get(i).name.compareToIgnoreCase(toInsert.name) == 0)){
-					if(toInsert.artist.compareToIgnoreCase(songList.get(i).artist) < 0) {
-						toInsertHelper(temp , toInsert);
-						return;
-					}
-					temp++;
-				}
-				toInsertHelper(temp , toInsert);
-				return;
-			}
-		}
-		 */
-		songList.add(toInsert);
-		listview.getItems().add(toInsert.name + " , " + toInsert.artist);
-	}
-
-
+	
+	/**
+	 * @param int i
+	 * @param  songObj toInsert
+	 * 
+	 * This method inserts the songObj in correct orders in arraylist
+	 * and listview
+	 */
 	public void toInsertHelper(int i, songObj toInsert) {
 		songList.add(i, toInsert);
 		listview.getItems().add(i, toInsert.name + " , " + toInsert.artist);
 	}
+
+
 	
-
-
+	/**
+	 * @param event
+	 * 
+	 * This method runs when the user presses the cancel button (To exit any event midway)
+	 */
 	@FXML
 	void cancelButton(ActionEvent event) {
 		clearTextFeilds();
 	}
 
 
-	
-	
-	// checks for file
-	public static boolean databaseAlreadyExists() {
-		File f = new File("DataBase.txt");
-		boolean exists = f.exists();
-		return exists;
-	}
 
 
+
+	/**
+	 * @return song Object
+	 * 
+	 * Gets the target object that has to be either edited or deleted
+	 */
 	public songObj getTargetObject() {
-
 		String [] words = (listview.getSelectionModel().getSelectedItem().toString()).split("\\s*,\\s*");
 		String name = words[0];
+		String artist = words[1];
 
 		songObj targetObj = null;
 
 		int size = songList.size();
 
 		for(int i =0; i<size; i++) {
-			if(name.equals(songList.get(i).name)) {
+			if((name.equals(songList.get(i).name)) && (artist.equals(songList.get(i).artist)) ) {
 				if(words.length > 1) {
 					if(words[1].toString().equals(songList.get(i).artist)) {
 						targetObj = songList.get(i);
@@ -329,6 +353,12 @@ public class ViewController {
 	}
 
 
+	
+	/**
+	 * @param event
+	 * 
+	 * This method views all the details about the song
+	 */
 	@FXML
 	void viewDetails(MouseEvent event) {
 
@@ -355,9 +385,7 @@ public class ViewController {
 				if(words.length > 1) {
 					if(words[1].toString().equals(songList.get(i).artist)) {
 						// Match found in array list
-
 						songObj targetObj = songList.get(i);
-
 						nameDetail.setText(targetObj.name);
 						artistDetail.setText(targetObj.artist);	
 						albumDetail.setText(targetObj.album);
@@ -383,6 +411,9 @@ public class ViewController {
 	}
 
 
+	/**
+	 * This method removes the selected song from the listview
+	 */
 	public void removeFromList() {
 
 		if(listview.getSelectionModel().isEmpty()) {
@@ -393,6 +424,29 @@ public class ViewController {
 
 
 
+
+	
+	/**
+	 * @return
+	 * 
+	 * This method checks if the database already exists. 
+	 * (File where we store the all the song info) 
+	 */
+	public static boolean databaseAlreadyExists() {
+		File f = new File("DataBase.txt");
+		boolean exists = f.exists();
+		return exists;
+	}
+
+
+	
+	
+	/**
+	 *  When the user closes the app
+	 *  we call this method to store the data in to Database.txt file
+	 *  
+	 * (As our text cases will be smaller we just made the file again)
+	 */
 	public static void  storeDataToFile () {
 		try {
 			Writer writer = new FileWriter("DataBase.txt");
@@ -409,37 +463,47 @@ public class ViewController {
 	}
 
 
-
-	// EDIT THIS ASAP
+	
+	/**
+	 * This method fills the arraylist of objects with all the data
+	 * from the database.txt when the app runs again after closing.
+	 */
 	public void retrieveDataFromFile() {
 		try {
 			String line;
 
-			FileReader fileReader = new FileReader("DataBase.txt");
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			FileReader file = new FileReader("DataBase.txt");
+			BufferedReader bufferedReader = new BufferedReader(file);
 
 			while ((line = bufferedReader.readLine()) != null ) {
 				String[] temp = line.split("\\t");
 
 				//  ADD TO ARRAYLIST 
-				/*
-				if (temp.length == 5) {
-					if (temp[4].equals("\n")) {
-						songObj tempObj = new songObj(temp[0], temp[1], temp[2], temp[3]);
-						songList.add(tempObj);
-					}
+				if(temp.length == 2) {
+					songObj tempObj = new songObj(temp[0], temp[1], "", "");
+					songList.add(tempObj);
+					listview.getItems().add(tempObj.name + " , " + tempObj.artist);
 				}
-				 */
 
+				if(temp.length >= 3) {
+					songObj tempObj = new songObj(temp[0], temp[1], temp[2], temp[3]);
+					songList.add(tempObj);
+					listview.getItems().add(tempObj.name + " , " + tempObj.artist);
+				}	
+			}
+			bufferedReader.close();
+			file.close();
+			if(!songList.isEmpty()) {
+				selectFirstSong();
+				viewDetailsHelper();
 			}
 
-			bufferedReader.close();
-			fileReader.close();
 		}
 		catch(IOException i) {
 			i.printStackTrace();
 		}
 	}
+
 
 }
 
